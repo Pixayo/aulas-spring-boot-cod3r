94110f3f531e0854ae6e5b2a11625a5ccd9eae18
Pixayo <kaio.rodrigo729@gmail.com>
2026-08-29 17:32:35 -0300

    aula: simulando erros
    Simula um erro de mapeamento ambíguo criando dois métodos para o mesmo método HTTP e rota.

--- src/main/java/cod3r/com/udemy/spring/controller/FirstController.java
+++ src/main/java/cod3r/com/udemy/spring/controller/FirstController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstController {
 
-    @GetMapping
+    // Simulação de erro: ambiguidade no mapeamento de rotas
+    // Ocorre quando dois métodos são mapeados para a mesma rota e possuem o mesmo verbo HTTP
+
+    // Neste caso, a rota "/intencional" e o método GET
+
+    @GetMapping(path = "/intencional")
     public String ola() {
+        return "Olá, esse é meu primeiro controller Spring Boot";
+    }
+
+    @GetMapping(path = "/intencional")
+    public String saudacao() {
         return "Olá, esse é meu primeiro controller Spring Boot";
     }
 
