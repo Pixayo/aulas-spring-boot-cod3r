cd4cd60e0e943e73952bd69756cdcfe4eabe968c
Pixayo <kaio.rodrigo729@gmail.com>
2026-08-29 17:05:13 -0300

    first commit

---
+++ src/test/java/cod3r/com/udemy/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package cod3r.com.udemy.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
