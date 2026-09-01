aace6bff85b9423a4e65dd837799ecc6660bf7a4
KAIO RODRIGUES TEIXEIRA <0023303@ALUNOS.UNICEPLAC.EDU.BR>
2026-09-01 09:32:07 -0300

    aula: recebendo parametros usando RequestParam e GET
    Modificamos o método mapeado em: "/clientes/{id}" para receber um parametro de requisição ao invés de apenas uma variável de caminho.

--- src/main/java/cod3r/com/udemy/spring/controller/FirstController.java
+++ src/main/java/cod3r/com/udemy/spring/controller/FirstController.java
@@ -3,10 +3,10 @@ package cod3r.com.udemy.spring.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cod3r.com.udemy.spring.model.Cliente;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -18,9 +18,17 @@ public class FirstController {
         return new Cliente(0, "nome", "123.456.789.10");
     }
 
+    // Nota: esta é uma forma mais adequada de conseguir valores através da URL
+    @GetMapping
+    public Cliente retornarClientePorId(@RequestParam int id) {
+        return new Cliente(id, "nome", "123.456.789.10");
+    }
+
+    /* Nota: esta forma de extrair valores da URL não é recomendado
     @GetMapping("/{id}")
     public Cliente retornarClientePorId(@PathVariable int id) {
         return new Cliente(id, "nome", "123.456.789.10");
     }
+    */
 
 }
